697426594185aa623db98945702d90e442b74fb8
Advait Soni <43613350+advaitsoni@users.noreply.github.com>
2020-05-05 13:35:08 -0300

    Add files via upload

---
+++ ping_devices without threading library.py
@@ -0,0 +1,42 @@
+import paramiko
+import time
+
+reachable_ip = []
+unreachable_ip = []
+
+def ping_device(host,password):
+
+    ssh_client = paramiko.SSHClient()
+    ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh_client.connect(hostname='10.0.12.1', username='advait', password=password)
+    remote_connection = ssh_client.invoke_shell()
+    cmd = 'ping ' + host + ' timeout 1 \n'
+    remote_connection.send(cmd)
+    time.sleep(2)
+    output = remote_connection.recv(65536)
+    output = output.decode()
+    temp_lst = output.split(':')
+    temp_lst2 = temp_lst[1].split('\n')
+    if temp_lst2[1].strip() == '.':
+        time.sleep(5)
+        unreachable_ip.append(host)
+    elif temp_lst2[1].strip() == '!!!!!':
+        reachable_ip.append(host)
+
+f = open('ip.txt', 'r')
+ip_list = []
+for i in f:
+    ip_list.append(i.strip())
+
+start = time.time()
+
+for ip in ip_list:
+    ping_device(ip, ‘cisco’)
+
+end = time.time()
+
+print 'total time: ' + str(end-start)
+print 'reachable ip address list: '
+print reachable_ip
+print 'unreachable ip address list: '
+print unreachable_ip
